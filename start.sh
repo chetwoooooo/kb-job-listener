ee6398e5e47ed50f414acd5c78e427288c7db05d
chetwoooooo <omerurrehman@gmail.com>
2025-10-15 15:19:48 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-set -eu
+set -e  # exit on error
 
-# Enable xtrace only if DEBUG=1 to reduce noise in prod
+# Enable xtrace only if DEBUG=1
 if [ "${DEBUG:-0}" = "1" ]; then
   set -x
 fi
 
 echo "start: launching listener"
 exec node kb-job-listener.mjs
-
