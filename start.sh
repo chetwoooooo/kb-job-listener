ac82d34c8287a64a87a45a169986adc929111a73
chetwoooooo <omerurrehman@gmail.com>
2025-10-15 15:15:41 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
-"filename": "start.sh", "language": "sh", "content": "#!/bin/sh\nset -eu\n# Enable xtrace only if DEBUG=1 to reduce noise in prod\nif [ "${DEBUG:-0}" = "1" ]; then\n set -x\nfi\necho "start: launching listener"\nexec node kb-job-listener.mjs\n"
+{
+  "filename": "start.sh",
+  "language": "sh",
+  "content": "#!/bin/sh\nset -eu\n# Enable xtrace only if DEBUG=1 to reduce noise in prod\nif [ \"${DEBUG:-0}\" = \"1\" ]; then\n  set -x\nfi\necho \"start: launching listener\"\nexec node kb-job-listener.mjs\n"
+}
 
